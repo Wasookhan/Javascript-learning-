acea803271c634c3180de9b6387937377cd69cdd
Wasii ullah <143061079+Wasookhan@users.noreply.github.com>
2023-12-31 08:33:29 +0000

    string in js

---
+++ string.js
@@ -0,0 +1,20 @@
+// let name = "Wasii ullah"
+// let age  = 20
+// detail = `My name is ${name} and i am ${age} years old`
+// console.log(detail)
+let name =new String("Wasii ullah")
+console.log(name[2])
+
+
+let cityName = "Peshawar"
+//string methods
+console.log(cityName.length) //length of string
+console.log(cityName.charAt(2))// character on which index
+console.log(cityName.concat(" is beautiful"))// add up strings
+console.log(cityName.includes("w"))// check whether the words includes or not 
+console.log(cityName.indexOf("P"))// check the index
+console.log(cityName.replace("Pesha", "Deba"))
+
+
+
+
